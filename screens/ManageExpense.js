20e10a59d04806917ff643911a1f780e43ecd66d
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-05-30 11:58:21 -0700

    adjusting code to use firebase id (i.e. name)

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -27,12 +27,12 @@ export default function ManageExpense({ route, navigation }) {
 		navigation.goBack();
 	}
 
-	function confirmHandler(expenseData) {
+	async function confirmHandler(expenseData) {
 		if (isEditing) {
 			expensesCtx.updateExpense(editedExpenseId, expenseData);
 		} else {
-			storeExpense(expenseData);
-			expensesCtx.addExpense(expenseData);
+			const id = await storeExpense(expenseData);
+			expensesCtx.addExpense({ ...expenseData, id: id });
 		}
 		navigation.goBack();
 	}
--- util/http.js
+++ util/http.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 const BACKEND_URL = "https://rnexpenses-7954e-default-rtdb.firebaseio.com";
 
-export function storeExpense(expenseData) {
-	axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
+export async function storeExpense(expenseData) {
+	const response = await axios.post(
+		`${BACKEND_URL}/expenses.json`,
+		expenseData
+	);
+	const id = response.data.name;
+	return id;
 }
 
 export async function fetchExpenses() {
