cbcfb64db58c40757d3d144adc8712f039b3b57b
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-10-04 11:49:11 -0700

    fix unseen text for ExpensesOutput screen

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,75 +1,74 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { Styles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
-import { Styles } from "../../constants/styles";
-
 export default function ExpenseItem({ id, description, amount, date }) {
-	const navigation = useNavigation();
+  const navigation = useNavigation();
 
-	function expensePressHandler() {
-		navigation.navigate("ManageExpense", {
-			expenseId: id,
-		});
-	}
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", {
+      expenseId: id,
+    });
+  }
 
-	return (
-		<Pressable
-			style={({ pressed }) => pressed && styles.pressed}
-			onPress={expensePressHandler}
-		>
-			<View style={styles.expenseItem}>
-				<View>
-					<Text style={[styles.textBase, styles.description]}>
-						{description}
-					</Text>
-					<Text style={styles.textBase}>{date.toDateString()}</Text>
-				</View>
-				<View style={styles.amountContainer}>
-					<Text style={styles.amount}>{amount.toFixed(2)}</Text>
-				</View>
-			</View>
-		</Pressable>
-	);
+  return (
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.expenseItem}>
+        <View>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
+        </View>
+      </View>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-	expenseItem: {
-		padding: 12,
-		marginVertical: 8,
-		backgroundColor: Styles.colors.primary500,
-		flexDirection: "row",
-		justifyContent: "space-between",
-		borderRadius: 6,
-		elevation: 3,
-		shadowColor: Styles.colors.gray500,
-		shadowRadius: 4,
-		shadowOffset: { width: 1, height: 1 },
-		shadowOpacity: 0.4,
-	},
-	textBase: {
-		color: Styles.colors.primary50,
-	},
-	description: {
-		fontSize: 16,
-		marginBottom: 4,
-		fontWeight: "bold",
-	},
-	amountContainer: {
-		paddingHorizontal: 12,
-		paddingVertical: 4,
-		backgroundColor: "white",
-		justifyContent: "center",
-		alignItems: "center",
-		borderRadius: 4,
-		minWidth: 80,
-	},
-	amount: {
-		color: Styles.colors.primary500,
-		fontWeight: "bold",
-	},
-	pressed: {
-		opacity: 0.5,
-	},
+  pressed: {
+    opacity: 0.75,
+  },
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: Styles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: Styles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: Styles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    minWidth: 80,
+  },
+  amount: {
+    color: Styles.colors.primary500,
+    fontWeight: "bold",
+  },
 });
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -6,35 +6,35 @@ import ExpensesSummary from "./ExpensesSummary";
 import { Styles } from "../../constants/styles";
 
 export default function ExpensesOutput({
-	expenses,
-	expensesPeriod,
-	fallbackText,
+  expenses,
+  expensesPeriod,
+  fallbackText,
 }) {
-	let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
 
-	if (expenses.length > 0) {
-		content = <ExpensesList expenses={expenses} />;
-	}
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
 
-	return (
-		<View style={styles.container}>
-			<ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-			{content}
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		paddingHorizontal: 24,
-		paddingTop: 24,
-		backgroundColor: Styles.colors.primary700,
-	},
-	infoText: {
-		color: "white",
-		fontSize: 16,
-		textAlign: "center",
-		marginTop: 32,
-	},
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    backgroundColor: Styles.colors.primary700,
+  },
+  infoText: {
+    color: Styles.colors.primary500,
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
+  },
 });
